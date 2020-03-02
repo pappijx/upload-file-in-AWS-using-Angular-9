78331afb5459d240bdef16b23c498e0c3f1181a6
Gajanan <gajushinde8046@gmail.com>
2020-03-02 21:26:36 +0530

    upload file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UploadService } from './upload.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 's3aws';
+export class AppComponent implements OnInit {
+
+    selectedFiles: FileList;
+    
+    constructor(private uploadService: UploadService) { }
+    
+    ngOnInit() {
+    }
+    
+    upload() {
+    const file = this.selectedFiles.item(0);
+    this.uploadService.uploadFile(file);
+    }
+    
+    selectFile(event) {
+    this.selectedFiles = event.target.files;
+    }
+      
 }
---
+++ src/app/upload.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import * as AWS from 'aws-sdk/global';
+import * as S3 from 'aws-sdk/clients/s3';
+@Injectable({
+  providedIn: 'root'
+})
+export class UploadService {
+
+  constructor() { }
+  uploadFile(file) {
+    const contentType = file.type;
+    const bucket = new S3(
+          {
+              accessKeyId: 'AKIAXJBTAKG5C66CEN6Z',
+              secretAccessKey: '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',
+              region: 'ap-south-1'
+          }
+      );
+      const params = {
+          Bucket: 'assetstoragemuse',
+          Key:  file.name,
+          Body: file,
+          ACL: 'public-read',
+          ContentType: contentType
+      };
+      bucket.upload(params, function (err, data) {
+          if (err) {
+              console.log('There was an error uploading your file: ', err);
+              return false;
+          }
+          console.log('Successfully uploaded file.', data);
+          return true;
+      });
+//for upload progress   
+/*bucket.upload(params).on('httpUploadProgress', function (evt) {
+          console.log(evt.loaded + ' of ' + evt.total + ' Bytes');
+      }).send(function (err, data) {
+          if (err) {
+              console.log('There was an error uploading your file: ', err);
+              return false;
+          }
+          console.log('Successfully uploaded file.', data);
+          return true;
+      });*/
+}
+}
